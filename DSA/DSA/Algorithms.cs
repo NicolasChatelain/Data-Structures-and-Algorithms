62d57a211cb3bd6fdf6c9b2391d44bd37d60b286
Daspudding7 <chatelain777@gmail.com>
2024-10-01 22:35:46 +0200

    add binarysearch

--- DSA/DSA/Algorithms.cs
+++ DSA/DSA/Algorithms.cs
@@ -2,6 +2,7 @@
 {
     public class Algorithms
     {
+        // Time complexity = O(N)
         public static bool LinearSearch(int[] integerArray, int value)
         {
             for (int i = 0; i < integerArray.Length; i++)
@@ -12,6 +13,36 @@
                 }
             }
             return false;
+        }
+
+        // Assume low is inclusive, high is exclusive
+        public static bool BinarySearch(int[] sortedIntegerArray, int value)
+        {
+            int low = 0;
+            int high = sortedIntegerArray.Length;
+
+            do
+            {
+                int midpoint = (int)Math.Floor((double)(low + (high - low) / 2));
+                var current = sortedIntegerArray[midpoint];
+
+                if (current == value)
+                {
+                    return true;
+                }
+                else if (current < value)
+                {
+                    low = midpoint + 1;
+                }
+                else
+                {
+                    high = midpoint;
+                }
+
+            } while (low < high);
+
+            return false;
+
         }
     }
 }
