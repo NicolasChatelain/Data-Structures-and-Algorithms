7eddbe9046339b583efebb4e07cbe214c40067e1
Daspudding7 <chatelain777@gmail.com>
2024-10-01 20:49:16 +0200

    add linear search

---
+++ DSA/DSA/Algorithms.cs
@@ -0,0 +1,17 @@
+﻿namespace DSA
+{
+    public class Algorithms
+    {
+        public static bool LinearSearch(int[] integerArray, int value)
+        {
+            for (int i = 0; i < integerArray.Length; i++)
+            {
+                if (integerArray[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
